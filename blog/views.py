edabcd1ea4b104e476e1b628c0277f86fa39470b
BPCollins <bpcollins1995@gmail.com>
2023-10-30 19:38:45 +0000

    Linking URLS but not working

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,4 @@ class PostList(generic.ListView):
     model = Post 
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     tempalte_name = 'index.html'
-    paginate_by = 6 
+    paginate_by = 6
